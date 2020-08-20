6c1aa0e47de675b9fa1c3b9aaed2d4cec0cb1f21
João Feck <joao@bravosul.com.br>
2020-08-20 04:17:00 -0300

    fix build ios

--- ios/Classes/CallKeepPlugin.m
+++ ios/Classes/CallKeepPlugin.m
@@ -1,8 +1,19 @@
 #import "CallKeepPlugin.h"
-#import <flutter_callkeep/flutter_callkeep-Swift.h>
 
 @implementation CallKeepPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftCallKeepPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"callkeep"
+            binaryMessenger:[registrar messenger]];
+  CallKeepPlugin* instance = [[CallKeepPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
 }
 @end
